21ae8ad85a9dea27705a1373c8a2b974a2060ff2
EduardaBrito0104 <dudabrito0104@gmail.com>
2023-06-05 09:14:05 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
@@ -0,0 +1,30 @@
+package com.itb.inf2hm.comercio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2hm.comercio.model.Produto;
+
+@Controller
+@RequestMapping("/comercio/produtos")
+
+public class LojaController {
+	
+	list<Produto> produtos = new ArraysList<Produto>
+	
+	@GetMapping("/listar")
+	public String listarProdutos(Model model){
+	
+		Produto produto = new Produto();
+		produto.setNome("Televisor 70' Samsung");
+		produto.setCodigoBarras("JFALJFDOE2514");
+		produto.setPreco("6541.25");
+		
+		
+		return "produtos";
+	
+		
+	}
+	
+}
---
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -0,0 +1,14 @@
+package com.itb.inf2hm.comercio.model;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes
+	// public : Propriedade (atributo ou método) so pode ser acessado
+	//          dentro da própria classe
+
+}
